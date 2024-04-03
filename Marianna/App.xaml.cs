1b79c54e6d78225a5f01649ca9a3abde0ac08e31
Mari <mariannasantiagogodoy58@gmail.com>
2024-04-03 10:49:21 -0300

    meu primeiro commit

---
+++ Marianna/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Marianna;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Marianna/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Marianna;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
