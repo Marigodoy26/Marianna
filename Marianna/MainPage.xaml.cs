05567aaa9ed73cfcdef68e6c19601206974275bc
Mari <mariannasantiagogodoy58@gmail.com>
2024-04-05 11:03:45 -0300

    botoes

--- Marianna/MainPage.xaml.cs
+++ Marianna/MainPage.xaml.cs
@@ -7,6 +7,27 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+	}
+
+	void Comer (object sender, EventArgs args)
+	{
+
+	}
+
+	void Beber (object sender, EventArgs args)
+	{
+		
+	}
+
+	void Brincar (object sender, EventArgs args)
+	{
+		
+	}
+
+	void Mudar (object sender, EventArgs args)
+	{
+		
 	}
 
 }
